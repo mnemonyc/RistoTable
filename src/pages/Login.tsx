2b0d23a081c821d98598cc50a2e45a5e0cb6684e
Francesco Colella <mnemonyc@hotmail.com>
2026-09-25 23:53:13 +0200

    Create Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { supabase } from '../lib/supabase'
+
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function login() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      alert(error.message)
+      return
+    }
+
+    alert('Login effettuato')
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>RistoTable</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={login}>
+        Accedi
+      </button>
+    </div>
+  )
+}
